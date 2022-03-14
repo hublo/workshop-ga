cf78e75b222ce0eb13e7953a7c8a453ec8091de4
Thomas Lathuiliere <thomas.lathuiliere@hublo.com>
2022-03-14 15:50:32 +0100

    ci: test package update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+var world = "world";
+function hello(world) {
+    return "Hello ".concat(world, "! ");
+}
+exports.hello = hello;
+console.log(hello(world));
